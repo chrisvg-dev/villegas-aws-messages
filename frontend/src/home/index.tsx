d701982c510a331eca292a5b00e389e6c32b398d
Cristhian Villegas Garcia <cristianvg9692@gmail.com>
2023-02-23 23:05:22 -0600

    Se sube campo de texto con evento

--- frontend/src/home/index.tsx
+++ frontend/src/home/index.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 export const Message = () => {
+    const sendMessage = () => {
+        alert('Click we');
+    }
+
     return (
-        <h1>Hello</h1>
+        <>
+            <section className='container'>
+                <h1>Envia SMS a los dispositivos registrados</h1>
+                <input onKeyUp={sendMessage} className='form-control' type="text" placeholder='Ingresa el mensaje' />
+            </section>
+        </>
     );
 }
