e3d6c175be3e5c6adef1c28cb4f6a356a6e6bb02
Cristhian Villegas Garcia <cristianvg9692@gmail.com>
2023-02-24 22:27:53 -0600

    Se sube conexion frontend - backend

--- backend/src/main/java/villegas/aws/messages/backend/controller/MessagesController.java
+++ backend/src/main/java/villegas/aws/messages/backend/controller/MessagesController.java
@@ -1,5 +1,18 @@
 package villegas.aws.messages.backend.controller;
 
-public class MessagesController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import villegas.aws.messages.backend.dto.CustomResponse;
 
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/messages")
+public class MessagesController {
+    @GetMapping
+    public ResponseEntity<?> index() {
+        CustomResponse response = new CustomResponse();
+        response.setMessage("Works");
+        return ResponseEntity.ok(response);
+    }
 }
--- frontend/src/home/index.tsx
+++ frontend/src/home/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 export const Message = () => {
-    const sendMessage = () => {
-        alert('Click we');
+    const url = 'http://localhost:9090/api/messages';
+
+    const sendMessage = async () => {
+        await fetch(url)
+            .then( response => response.json() )
+            .then( jsonResponse => alert(jsonResponse.message) );
     }
 
     return (
