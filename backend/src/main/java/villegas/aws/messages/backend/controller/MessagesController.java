68e4adad4cf970da05132a7aab46a6f2ba22ec7f
Cristhian Villegas Garcia <cristianvg9692@gmail.com>
2023-02-28 23:04:13 -0600

    Se sube el servicio para aws

--- backend/src/main/java/villegas/aws/messages/backend/controller/MessagesController.java
+++ backend/src/main/java/villegas/aws/messages/backend/controller/MessagesController.java
@@ -1,19 +1,28 @@
 package villegas.aws.messages.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import villegas.aws.messages.backend.dto.CustomRequest;
 import villegas.aws.messages.backend.dto.CustomResponse;
+import villegas.aws.messages.backend.service.AwsMessageService;
+import villegas.aws.messages.backend.service.AwsMessageServiceImpl;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/messages")
 public class MessagesController {
+    private AwsMessageService awsMessageService;
+
+    public MessagesController(AwsMessageService awsMessageService){
+        this.awsMessageService = awsMessageService;
+    }
+
     @PostMapping
     public ResponseEntity<?> index(@RequestBody CustomRequest request) {
         CustomResponse response = new CustomResponse();
-        response.setMessage( request.getMessage() );
+        this.awsMessageService.sendMessage( request.getMessage() );
         return ResponseEntity.ok(response);
     }
 }
